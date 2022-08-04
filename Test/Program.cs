4b999dbea43ecbec81429901cbdfa65fb0202469
Anatoly Finchenko <finchenko777@gmail.com>
2022-08-04 21:00:01 +0300

    *Второй_комит_задача_решена*

--- Test/Program.cs
+++ Test/Program.cs
@@ -9,4 +9,14 @@ for (int i = 0; i < array.Length; i++)
     if (array[i].Length <= 3) count +=1;
 }
 System.Console.WriteLine($" [ {string.Join(", ", array)} ]");
-System.Console.WriteLine(count);
+string[] sorting = new string[count];
+int index = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if  (array[i].Length <= 3) 
+    {
+        sorting[index] = array[i];
+        index += 1;
+    }
+}
+System.Console.WriteLine( "[ " + string.Join(" ," , sorting) + " ]");
